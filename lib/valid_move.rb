ca24e97dff7daf2417f97e01e3aa39556d3dacd7
Shannon Young <shannon.ari1@gmail.com>
2020-09-12 00:23:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 
 
 board = ["",""," "," "," "," "," "," "," "]
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+end
+
 def position_taken?(board, index)
  !(board[index] == " " || board[index] == "" || board[index] == nil)
 end
